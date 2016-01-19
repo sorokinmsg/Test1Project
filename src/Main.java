ea300fd4d849f709eb4c2b4dfa319ec371226835
sorokinmsg <sorokinmsg@gmail.com>
2016-01-19 17:33:39 +0300

    n2

--- src/Main.java
+++ src/Main.java
@@ -59,6 +59,10 @@ public class Main {
         int[][] A = new int[10][10];
 
 
+
+        tt.toastPrintLn("---Матрица случайных чисел---");
+
+
         for (int i=0; i<10; i++){
             for (int j=0; j<10; j++){
                 A[i][j] = (int)(Math.random()*99)+1;
@@ -67,6 +71,91 @@ public class Main {
             }
             tt.toastPrintLn("");
         }
+
+
+tt.toast_space();
+        tt.toastPrintLn("---Матрица четных элементов---");
+
+    for (int i=0; i<10; i++){
+        for (int j=0; j<10; j++){
+            if(A[i][j]%2 == 0){
+                tt.toastPrint(1 + "\t");
+            }
+            else {
+                tt.toastPrint(0 + "\t");
+            }
+        }
+        tt.toastPrintLn("");
+    }
+
+
+
+        tt.toast_space();
+        tt.toastPrintLn("---Обратная матрица---");
+
+    for (int i=9; i>=0; i--){
+        for (int j=9; j>=0; j--){
+
+            tt.toastPrint(A[i][j] + "\t");
+        }
+        tt.toastPrintLn("");
+    }
+
+        tt.toast_space();
+
+        tt.toastPrintLn("---Транспонированная матрица---");
+
+        for (int i=0; i<10; i++){
+            for (int j=0; j<10; j++){
+
+                tt.toastPrint(A[j][i] + "\t");
+            }
+            tt.toastPrintLn("");
+        }
+        tt.toast_space();
+        tt.toast_space();
+
+        int min;
+        int temp;
+
+
+        for (int i=0; i<10; i++){
+            for (int j=0; j<10; j++){
+
+
+                for (int l=0; l<10; l++){
+                    for (int k=0; k<10; k++){
+                    if(A[i][j]<A[l][k]){
+                        min = A[l][k];
+                        temp = A[i][j];
+
+                        A[i][j] = min;
+                        A[l][k] = temp;
+                    }
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        tt.toastPrintLn("---Упорядоченная матрица---");
+
+        for (int i=0; i<10; i++){
+            for (int j=0; j<10; j++){
+
+                tt.toastPrint(A[i][j] + "\t");
+            }
+            tt.toastPrintLn("");
+        }
+
+
+
+
+
+
     }
 
 
